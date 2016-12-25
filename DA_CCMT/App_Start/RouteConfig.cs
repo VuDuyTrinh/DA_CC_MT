de15a2f6777fac33514e288262754407009f1ec4
nguyen146 <shjratori@gmail.com>
2016-12-25 14:50:34 +0700

    Code tự sinh (Trang chủ: index)

--- DA_CCMT/App_Start/RouteConfig.cs
+++ DA_CCMT/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace DA_CCMT
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "TrangChu", action = "Index", id = UrlParameter.Optional } //Truyền Controller và action khi mở trang chủ
             );
         }
     }
---
+++ DA_CCMT/Controllers/TrangChuController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PagedList;   // Sử dụng PagedList để phân trang
+using PagedList.Mvc;
+using DA_CCMT.Models;
+
+namespace DA_CCMT.Controllers
+{
+    public class TrangChuController : Controller
+    {
+        QLNHDataContext data = new QLNHDataContext(); // Biến data quản lý CSDL
+        private List<SanPham> LaySP()
+        {
+            return data.SanPhams.ToList(); // hàm trả về list table SanPham
+        }
+
+        public ActionResult Index(int? page ) // Khi nhấn vào trang chủ hàm này sẽ kích hoạt
+        {                                      //tham số page để chỉ ra số trang trong list sản phẩm ở trang chủ
+            int SoSP = 9; // SoSP chỉ ra số sản phẩm sẽ hiển thị trên web, nếu quá hơn số sp sẽ tự phân thành trang kế tiếp             
+            int SoTrang = (page ?? 1); //Chỉ ra số trang 
+
+            var nh = LaySP(); //Lấy list sản phẩm có trong CSDL
+            return View(nh.ToPagedList(SoTrang, SoSP)); //Trả về Model chứa table SanPham, chứa hàm phân trang có trong PagedList
+        }
+
+
+        public ActionResult Hang() 
+        {
+            var hang = (from a in data.HangNHs select a).Take(12); //Lấy 12 sản phẩm
+            return PartialView(hang);
+        }
+    }
+}
