fb8c1caefb26bdb1d6aee31fc5644abae8f0a91a
QuangQT <QuangQT>
2016-12-28 02:02:04 +0700

    commit don dat hang

---
+++ DA_CCMT/Models/ddh.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using webfinal.Models;
+
+namespace webfinal.Models
+{
+    public class ddh
+    {
+        QLNHDataContext data = new QLNHDataContext();
+        public int iMaDDH { set; get; }
+        public string iThanhToan { set; get; }
+        public string iNgayDat { set; get; }
+        public string iNgayGiao { set; get; }
+        public string iGiaoHang { set; get; }
+        public KhachHang ikhachhang { set; get; }
+        public ddh(int id)
+        {
+            DonDatHang dh = data.DonDatHangs.SingleOrDefault(n => n.MADDH == id);
+            iMaDDH = dh.MADDH;
+            iThanhToan = dh.DaThanhToan.ToString();
+            iNgayDat = dh.NgayDat.ToString();
+            iNgayGiao = dh.NgayGiao.ToString();
+            iGiaoHang = dh.TinhTrangGiaoHang.ToString();
+            ikhachhang = data.KhachHangs.SingleOrDefault(m => m.MaKH == dh.MaKH);
+        }
+    }
+}
