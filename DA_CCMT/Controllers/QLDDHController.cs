93da7bb6952e867ef03c38f3a182522653d9c48b
vitcon272393 <badaopro.2011.nvt@gmail.com>
2016-12-31 14:06:57 +0700

    Danh Sach Don Dat Hang QuanLyDonDatHang

---
+++ DA_CCMT/Controllers/QLDDHController.cs
@@ -0,0 +1,22 @@
+﻿using DA_CCMT.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DA_CCMT.Controllers
+{
+    public class QLDDHController : Controller
+    {
+        // GET: QLDDH
+        QLNHDataContext data = new QLNHDataContext();
+        public ActionResult Index()
+        {
+            //nếu cờ =1 thì lấy danh sách các đơn đặt hàng
+            var ds = from a in data.DonDatHangs select a;
+            return View(ds);
+
+        }
+    }
+}
