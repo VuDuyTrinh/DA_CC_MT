c75d974d18a0b8a0d06ca1b984363493123d4b27
thanhquangqb95 <thanhquangqb95@gmail.com>
2017-01-07 02:53:00 +0700

    quản lý tài khoản

--- DA_CCMT/Controllers/QLTKController.cs
+++ DA_CCMT/Controllers/QLTKController.cs
@@ -18,7 +18,6 @@ namespace DA_CCMT.Controllers
             {
                 return RedirectToAction("Login", "Admin");
             }
-<<<<<<< HEAD
             int co = 0;
             String tencn = "";
             List<CnAd> cn = Session["CN"] as List<CnAd>;
@@ -35,24 +34,6 @@ namespace DA_CCMT.Controllers
 
                 return RedirectToAction("Loi", "Admin");
             }
-=======
-            //int co = 0;
-            //String tencn = "";
-            //List<CnAd> cn = Session["CN"] as List<CnAd>;
-            //foreach (CnAd i in cn)
-            //{
-            //    if (i.iAction == "QLTK" && i.iController == "QLTK")
-            //    {
-            //        tencn = i.iTenCN;
-            //        co = 1;
-            //    }
-            //}
-            //if (co == 0)
-            //{
-
-            //    return RedirectToAction("Loi", "Admin");
-            //}
->>>>>>> origin/master
             var ds = from a in data.Admins select a;
             return View(ds);
         }
@@ -62,7 +43,6 @@ namespace DA_CCMT.Controllers
             {
                 return RedirectToAction("Login", "Admin");
             }
-<<<<<<< HEAD
             int co = 0;
             String tencn = "";
             List<CnAd> cn = Session["CN"] as List<CnAd>;
@@ -79,24 +59,6 @@ namespace DA_CCMT.Controllers
 
                 return RedirectToAction("Loi", "Admin");
             }
-=======
-            //int co = 0;
-            //String tencn = "";
-            //List<CnAd> cn = Session["CN"] as List<CnAd>;
-            //foreach (CnAd i in cn)
-            //{
-            //    if (i.iAction == "QLTK" && i.iController == "QLTK")
-            //    {
-            //        tencn = i.iTenCN;
-            //        co = 1;
-            //    }
-            //}
-            //if (co == 0)
-            //{
-
-            //    return RedirectToAction("Loi", "Admin");
-            //}
->>>>>>> origin/master
             Admin a = data.Admins.SingleOrDefault(n => n.TenTK == id);
             ViewBag.TenTK = a.TenTK;
             ViewBag.HoTen = a.HoTen;
@@ -116,7 +78,6 @@ namespace DA_CCMT.Controllers
             {
                 return RedirectToAction("Login", "Admin");
             }
-<<<<<<< HEAD
             int co = 0;
             String tencn = "";
             List<CnAd> cn = Session["CN"] as List<CnAd>;
@@ -133,52 +94,34 @@ namespace DA_CCMT.Controllers
 
                 return RedirectToAction("Loi", "Admin");
             }
-=======
-            //int co = 0;
-            //String tencn = "";
-            //List<CnAd> cn = Session["CN"] as List<CnAd>;
-            //foreach (CnAd i in cn)
-            //{
-            //    if (i.iAction == "QLTK" && i.iController == "QLTK")
-            //    {
-            //        tencn = i.iTenCN;
-            //        co = 1;
-            //    }
-            //}
-            //if (co == 0)
-            //{
-
-            //    return RedirectToAction("Loi", "Admin");
-            //}
->>>>>>> origin/master
             var a = from b in data.ChucNangs select b;
             return View(a);
         }
         [HttpPost]
         public ActionResult taotk(FormCollection collection)
         {
-             var a = from b in data.ChucNangs select b;
+            var a = from b in data.ChucNangs select b;
             var tentk = collection["TenTK"];
             var hoten = collection["HoTen"];
             var mk = collection["MK"];
             var mk2 = collection["MK2"];
-            if(tentk==""||hoten==""||mk==""||mk2=="")
+            if (tentk == "" || hoten == "" || mk == "" || mk2 == "")
             {
                 ViewBag.ThongBao = "Hãy nhập đầy đủ thông tin";
                 return View(a);
             }
-            if(mk!=mk2)
+            if (mk != mk2)
             {
                 ViewBag.ThongBao = "Nhập lại mật khẩu sai";
                 return View(a);
             }
-            if (mk.Length<5)
+            if (mk.Length < 5)
             {
                 ViewBag.ThongBao = "mật khẩu phải lớn hơn 5 ký tự";
                 return View(a);
             }
             var tam = data.Admins.SingleOrDefault(n => n.TenTK == tentk);
-            if(tam!=null)
+            if (tam != null)
             {
                 ViewBag.ThongBao = "Tên tài khoản đã tồn tại";
                 return View(a);
@@ -189,10 +132,10 @@ namespace DA_CCMT.Controllers
             ad.TenTK = tentk;
             data.Admins.InsertOnSubmit(ad);
             data.SubmitChanges();
-            
-            foreach(var i in a)
+
+            foreach (var i in a)
             {
-                if(collection[i.TenCN.ToString()]==i.TenCN.ToString())
+                if (collection[i.TenCN.ToString()] == i.TenCN.ToString())
                 {
                     CTCN ct = new CTCN();
                     ct.MaCN = i.MaCN;
@@ -203,5 +146,5 @@ namespace DA_CCMT.Controllers
             }
             return RedirectToAction("Index");
         }
-	}
+    }
 }
