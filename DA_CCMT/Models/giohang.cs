6da2ef9e4d7a1361ad7d1991cd81837aaec65484
quangqt <quagqt1995@gmail.com>
2017-01-06 23:31:48 +0700

    add_model_GioHang

---
+++ DA_CCMT/Models/giohang.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace webfinal.Models
+{
+    public class giohang
+    {
+        QLNHDataContext data = new QLNHDataContext();
+        public int iMANH { set; get; }
+        public String iHinh { set; get; }
+        public String iTenNH { set; get; }
+        public int iGia { set; get; }
+        public int iSoLuong { set; get; }
+        public Double iThanhTie { get { return iSoLuong * iGia; } }
+        public giohang(int MANH, int sl)
+        {
+            iMANH = MANH;
+            SanPham nh = data.SanPhams.Single(n => n.MaNH == iMANH);
+            iHinh = nh.hinh;
+            iTenNH = nh.TenNH;
+            iGia = int.Parse(nh.Gia.ToString());
+            iSoLuong = sl;
+        }
+    }
+}
