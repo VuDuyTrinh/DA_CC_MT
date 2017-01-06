45132b1bce357c3a4272cf70160fff05f10c1ebc
Lê Chính <lxchinh89@gmail.com>
2017-01-06 18:17:45 +0700

    Thống kê tài khoản đăng kí

---
+++ DA_CCMT/Controllers/ThongKeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using webfinal.Models;
+
+namespace webfinal.Controllers
+{
+    public class ThongKeController : Controller
+    {
+        //
+        // GET: /ThongKe/
+        QLNHDataContext data = new QLNHDataContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult taikhoan()
+        {
+            int i = data.Admins.Count();
+            ViewBag.SL = i;
+            return View(i);
+        }
+    }
