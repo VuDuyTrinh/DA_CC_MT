80f88d9f7adc7cde21c570ce1fd52667717ba7a2
vitcon272393 <badaopro.2011.nvt@gmail.com>
2017-01-06 13:32:23 +0700

    dsddh vs2 QuanLyDonDatHang

--- DA_CCMT/Controllers/QLDDHController.cs
+++ DA_CCMT/Controllers/QLDDHController.cs
@@ -13,6 +13,30 @@ namespace DA_CCMT.Controllers
         QLNHDataContext data = new QLNHDataContext();
         public ActionResult Index()
         {
+            //kiem tra xem admin nếu có thực hiện kiểm tra quyền ở phần phân quyền
+            if (Session["Admin"] == null)
+            {
+                return RedirectToAction("Login", "Admin");
+            }
+            int co = 0;
+            String tencn = "";
+            //lấy các chức năng của tài khoản admin đăng nhập
+            List<CnAd> cn = Session["CN"] as List<CnAd>;
+            foreach (CnAd i in cn)
+            {
+                //kiểm tra xem tai khoản có quyền quản lý đơn đặt hàng k?
+                if (i.iAction == "DDH" && i.iController == "DDH")
+                {
+                    tencn = i.iTenCN;
+                    //nếu có gán biên bằng =1
+                    co = 1;
+                }
+            }
+            //kiểm tra biên cờ
+            if (co == 0)
+            {
+                return RedirectToAction("Loi", "Admin");
+            }
             //nếu cờ =1 thì lấy danh sách các đơn đặt hàng
             var ds = from a in data.DonDatHangs select a;
             return View(ds);
