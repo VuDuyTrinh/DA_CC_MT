260f89547ed469e9d57b554557be929322f1da1b
QuangQT <QuangQT>
2017-01-06 21:40:36 +0700

    Tu_van_controller

--- DA_CCMT/Controllers/TrangChuController.cs
+++ DA_CCMT/Controllers/TrangChuController.cs
@@ -1,10 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using PagedList;   // Sử dụng PagedList để phân trang
 using PagedList.Mvc;
+using System;
 using DA_CCMT.Models;
 
 namespace DA_CCMT.Controllers
@@ -62,6 +62,59 @@ namespace DA_CCMT.Controllers
             int SoTrang = (page ?? 1);
             var sp = from a in data.SanPhams where (a.MaHang == id) select a;
             return View(sp.ToPagedList(SoTrang, SoSP));
+        }
+        public List<tuvan> laytuvan()
+        {
+            List<tuvan> tv = new List<tuvan>();
+            var a = from b in data.CauHois orderby b.MaCauHoi descending select b;
+
+            foreach (var item in a)
+            {
+                tuvan t = new tuvan(item.MaCauHoi);
+                tv.Add(t);
+            }
+            return tv;
+        }
+        public ActionResult TuVan(int? pape)
+        {
+            List<tuvan> tv = laytuvan();
+            int SoSP = 9;
+            int SoTrang = (pape ?? 1);
+            return View(tv.ToPagedList(SoTrang, SoSP));
+        }
+        [HttpPost]
+        public ActionResult TuVan(int? pape, FormCollection colection)
+        {
+            List<tuvan> tv = laytuvan();
+            int SoSP = 9;
+            int SoTrang = (pape ?? 1);
+            if (Session["TaiKhoan"] == null)
+            {
+                ViewBag.ThongBao = "Bạn phải đăng nhập để gửi thư";
+                return View(tv.ToPagedList(SoTrang, SoSP));
+            }
+            else
+            {
+                string cauhoi = colection["cauhoi"];
+                if (cauhoi == "")
+                {
+                    ViewBag.ThongBao = "Hãy nhập câu hỏi";
+                    return View(tv.ToPagedList(SoTrang, SoSP));
+                }
+                else
+                {
+                    KhachHang kh = (KhachHang)Session["TaiKhoan"];
+                    CauHoi c = new CauHoi();
+                    c.MaKH = kh.MaKH;
+                    c.NoiDung = cauhoi;
+                    c.TraLoi = 0;
+                    data.CauHois.InsertOnSubmit(c);
+                    data.SubmitChanges();
+                    tv = laytuvan();
+                    return View(tv.ToPagedList(SoTrang, SoSP));
+                }
+            }
+            return View(tv.ToPagedList(SoTrang, SoSP));
         }
     }
 }
